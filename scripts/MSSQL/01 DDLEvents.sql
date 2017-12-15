7487438c5c88435b207b4b409a0a1052b52309a2
hellgorithm <jimbertamante@gmail.com>
2017-12-15 14:29:23 +0800

    added objecttype in columns

--- scripts/MSSQL/01 DDLEvents.sql
+++ scripts/MSSQL/01 DDLEvents.sql
@@ -3,16 +3,18 @@ USE [SQLVC]
 
 
 CREATE TABLE [dbo].[DDLEvents](
+	[RowID] [int] IDENTITY(1,1) NOT NULL,
 	[EventType] [varchar](max) NOT NULL,
 	[EventDDL] [varchar](max) NULL,
 	[DatabaseName] [varchar](max) NULL,
 	[SchemaName] [varchar](max) NULL,
 	[ObjectName] [varchar](max) NULL,
+	[ObjectType] [varchar](max) NULL,
 	[HostName] [varchar](max) NULL,
 	[IPAddress] [varchar](max) NULL,
 	[ProgramName] [varchar](max) NULL,
 	[LoginName] [varchar](max) NULL,
-	[EventDate] [datetime] default GETDATE()
+	[EventDate] [datetime] NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
 
